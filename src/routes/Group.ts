b411bed1863df697ecf15243dcaa2001c431c452
Botond Demeter <murthagorvok@gmail.com>
2023-04-15 20:00:52 +0200

    Fix group routes

--- src/routes/Group.ts
+++ src/routes/Group.ts
@@ -5,10 +5,10 @@ import verifyToken from '../middleware/VerifyToken';
 const router = express.Router();
 
 router.post('/create', verifyToken, controller.createGroup);
-router.get('/get/:eventID', controller.readGroup);
+router.get('/get/:groupID', controller.readGroup);
 router.get('/get_for_user/', verifyToken, controller.readGroupsForUser);
 router.get('/get_all/', controller.readAllGroups);
-router.patch('/update/:eventID', verifyToken, controller.updateGroup);
-router.delete('/delete/:eventID', verifyToken, controller.deleteGroup);
+router.patch('/update/:groupID', verifyToken, controller.updateGroup);
+router.delete('/delete/:groupID', verifyToken, controller.deleteGroup);
 
 export = router;
